c7ae8d1f480b590cec97a560229c3e56493dcdf9
marcin-p-b <162914885+marcin-p-b@users.noreply.github.com>
2024-05-25 00:28:21 +0200

    Add files via upload

--- src/components/ShowRepositories.jsx
+++ src/components/ShowRepositories.jsx
@@ -6,35 +6,31 @@ export default function ShowRepositories({ isLoading, repoData }) {
   // If data loaded and user has any public repositories then display the repositories, otherwise display message "User has no public repositories"
   return (
     <div className="show-repo-container">
-      <ul>
-        {!isLoading ? (
-          repoData.length !== 0 ? (
-            repoData.map((repositoryData) => {
-              // Map repositories of a user
-              return (
-                <div key={repositoryData.id}>
-                  <div className="repo-container">
-                    <li className="show-repo-list">
-                      <a
-                        href={repositoryData.html_url}
-                        rel="noopener"
-                        target="_blank"
-                      >
-                        {repositoryData.name}
-                      </a>
-                    </li>
-                    <Statistics data={repositoryData} />
-                  </div>
-                  <Details data={repositoryData} />
-                  <hr />
+      {!isLoading ? (
+        repoData.length !== 0 ? (
+          repoData.map((repositoryData) => {
+            // Map repositories of a user
+            return (
+              <div key={repositoryData.id}>
+                <div className="repo-container">
+                  <a
+                    href={repositoryData.html_url}
+                    rel="noopener"
+                    target="_blank"
+                  >
+                    {repositoryData.name}
+                  </a>
+                  <Statistics data={repositoryData} />
                 </div>
-              );
-            })
-          ) : (
-            <label>User has no public repositories</label>
-          )
-        ) : null}
-      </ul>
+                <Details data={repositoryData} />
+                <hr />
+              </div>
+            );
+          })
+        ) : (
+          <label>User has no public repositories</label>
+        )
+      ) : null}
     </div>
   );
 }
